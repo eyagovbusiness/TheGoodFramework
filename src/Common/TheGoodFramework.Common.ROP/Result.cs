1c26cf1df2a8d4bb3c8edd58d97fba11c8c54ab2
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-04-05 15:30:41 +0200

    Added Result following RailwayOrientedProgramming pattern to common libraries.

---
+++ src/Common/TheGoodFramework.Common.ROP/Result.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+
+namespace TGF.Common.ROP
+{
+
+    /// <summary>
+    /// Base class of <see cref="Result{T}"/> without Value.
+    /// </summary>
+    public class ResultBase
+    {
+        public readonly ImmutableArray<Error> ErrorList;
+        public bool IsSuccess => ErrorList.Length == 0;
+
+        public ResultBase()
+        {
+            ErrorList = ImmutableArray<Error>.Empty;
+        }
+
+        public ResultBase(ImmutableArray<Error> aErrorList)
+        {
+            if (aErrorList.Length == 0)
+                throw new InvalidOperationException("Can't create a failure Result without errors.");
+
+            ErrorList = aErrorList;
+        }
+
+    }
+
+    /// <summary>
+    /// Class that represents the unit of information while returning the result of operations in Reailway Oriented Programming.
+    /// </summary>
+    /// <typeparam name="T">Type of the result Value.</typeparam>
+    public class Result<T> : ResultBase
+    {
+        public readonly T Value;
+        public Result(T aValue) : base()
+        {
+            Value = aValue;
+        }
+        public Result(ImmutableArray<Error> aErrorList) : base(aErrorList)
+        {
+            Value = default;
+        }
+
+    }
+
+    /// <summary>
+    /// Static class to create new instances of Results with success or failure(s).
+    /// </summary>
+    public static class Result
+    {
+        public static Result<T> Success<T>(this T aValue) => new Result<T>(aValue);
+
+        public static Result<T> Failure<T>(ImmutableArray<Error> aErrorList) => new Result<T>(aErrorList);
+
+        public static Result<T> Failure<T>(Error aError) => new Result<T>(ImmutableArray.Create(aError));
+    }
+
+}
