20fbe42cfbdc7f780627c46e3d0036be937138f1
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-04-11 12:14:51 +0200

    Updated ROP library to support proper serialization of Result objects.

--- src/Common/TheGoodFramework.Common.ROP/Errors/HttpError.cs
+++ src/Common/TheGoodFramework.Common.ROP/Errors/HttpError.cs
@@ -1,20 +1,25 @@
-﻿using System.Net;
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json.Serialization;
 
 namespace TGF.Common.ROP.Errors
 {
     /// <summary>
     /// Public interface of any HttpError
     /// </summary>
+    [JsonObject]
     public interface IHttpError
     {
+        [JsonPropertyName("Error")]
         IError Error { get; }
+        [JsonPropertyName("StatusCode")]
         HttpStatusCode StatusCode { get; }
     }
 
     /// <summary>
     /// Struct of an HttpError.
     /// </summary>
-    public readonly struct HttpError : IHttpError
+    public class HttpError : IHttpError
     {
         public IError Error { get; }
         public HttpStatusCode StatusCode { get; }
