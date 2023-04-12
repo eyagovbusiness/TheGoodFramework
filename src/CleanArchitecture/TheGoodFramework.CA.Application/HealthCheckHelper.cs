498ac34ea5058b7077873a0cc9bcaed523dd2267
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-04-12 17:50:38 +0200

    Fixed wrong project paths after TGF library projects structure refactor.

---
+++ src/CleanArchitecture/TheGoodFramework.CA.Application/HealthCheckHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TGF.CA.Application.Setup
+{
+    /// <summary>
+    /// Class to help building HealthChecksUI configuration.
+    /// </summary>
+    public static class HealthCheckHelper
+    {
+        /// <summary>
+        /// Builds a new <see cref="IConfiguration"/> from in-memory stored HealthChecksUI configuration.
+        /// </summary>
+        /// <param name="aAditionalHealtCheckConfig">Aditional JSON configuration to add in Dictionary format.</param>
+        /// <returns><see cref="IConfiguration"/> with added HealthChecksUI configuration from memory.</returns>
+        public static IConfiguration BuildBasicHealthCheck(Dictionary<string, string?>? aAditionalHealtCheckConfig = null)
+        {
+            var lNewConfigurationBuilder =
+                new ConfigurationBuilder()
+                .AddInMemoryCollection(
+                new Dictionary<string, string?>
+                {
+                    {"HealthChecksUI:HealthChecks:0:Name", "self"},
+                    {"HealthChecksUI:HealthChecks:0:Uri", $"http://localhost/health"},
+                    {"HealthChecksUI:EvaluationTimeInSeconds", "60" },
+                    {"HealthChecksUI:MinimumSecondsBetweenFailureNotifications", "90" }
+                });
+
+            return aAditionalHealtCheckConfig == null
+                ? lNewConfigurationBuilder.Build()
+                : lNewConfigurationBuilder.AddInMemoryCollection(aAditionalHealtCheckConfig).Build();
+
+        }
+    }
+}
