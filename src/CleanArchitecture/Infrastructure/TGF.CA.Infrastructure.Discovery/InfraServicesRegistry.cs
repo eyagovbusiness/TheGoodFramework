a21c81bd38a19ebbe1177e598af4a835cbcd1829
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-04-28 23:26:15 +0200

    Added service discovery abtractions.

---
+++ src/CleanArchitecture/Infrastructure/TGF.CA.Infrastructure.Discovery/InfraServicesRegistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGF.CA.Infrastructure.Discovery
+{
+    public class InfraServicesRegistry
+    {
+        public const string MessageBroker = "RabbitMQ";
+        public const string VaultSecretsManager = "VaultSecretsManager";
+        public const string MySql = "MySql";
+    }
+}
