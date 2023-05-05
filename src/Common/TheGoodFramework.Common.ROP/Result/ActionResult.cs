e8c6729cb00161fb0d94d73274f883a4e8ff9a48
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-05-05 14:52:57 +0200

    ActionResult extensions rework to pick from Result value or Errors depending on the success or failure during the operation.

--- src/Common/TheGoodFramework.Common.ROP/Result/ActionResult.cs
+++ src/Common/TheGoodFramework.Common.ROP/Result/ActionResult.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using TGF.Common.ROP.HttpResult;
+using TGF.Common.ROP.Errors;
 
 namespace TGF.Common.ROP.Result
 {
@@ -18,31 +19,7 @@ namespace TGF.Common.ROP.Result
         {
             public ResultWithStatusCode(T aContext, HttpStatusCode aStatusCode)
                 : base(aContext)
-            {
-                base.StatusCode = (int)aStatusCode;
-            }
-        }
-
-        /// <summary>
-        /// Gets a new instance of an <see cref="IActionResult"/> created from the provided <paramref name="aHttpResult"/>.
-        /// </summary>
-        /// <typeparam name="T">Type of the Value propery from <see cref="IHttpResult{T}"/>.</typeparam>
-        /// <param name="aHttpResult">An instance of <see cref="IHttpResult{T}"/>.</param>
-        /// <returnsawaitable <see cref="Task{IActionResult}"/>.></returns>
-        public static IActionResult ToActionResult<T>(this IHttpResult<T> aHttpResult)
-        {
-            return ((IResult<T>)aHttpResult).ToHttpStatusCode(aHttpResult.StatusCode);
-        }
-
-        /// <summary>
-        /// Gets a <see cref="Task"/> that returns a new instance of an <see cref="IActionResult"/> created from the provided <paramref name="aHttpResult"/>.
-        /// </summary>
-        /// <typeparam name="T">Type of the Value propery from <see cref="IHttpResult{T}"/>.</typeparam>
-        /// <param name="aHttpResult">An instance of <see cref="IHttpResult{T}"/>.</param>
-        /// <returns>awaitable <see cref="Task{IActionResult}"/>.</return
-        public static async Task<IActionResult> ToActionResult<T>(this Task<IHttpResult<T>> aHttpResult)
-        {
-            return (await aHttpResult).ToActionResult();
+                => base.StatusCode = (int)aStatusCode;
         }
 
         /// <summary>
@@ -52,21 +29,37 @@ namespace TGF.Common.ROP.Result
         /// <param name="aHttpResult">An instance of <see cref="IHttpResult{T}"/> as <see cref="IResult{T}"/>.</param>
         /// <returns>awaitable <see cref="Task{IActionResult}"/>.</returns>
         public static async Task<IActionResult> ToActionResult<T>(this Task<IResult<T>> aHttpResult)
-        {
-            return (await aHttpResult).TryHttpResultParse().ToActionResult();
-        }
+            => (await aHttpResult).TryHttpResultParse().ToActionResult();
+
+        /// <summary>
+        /// Gets a new instance of an <see cref="IActionResult"/> created from the provided <paramref name="aHttpResult"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the Value propery from <see cref="IHttpResult{T}"/>.</typeparam>
+        /// <param name="aHttpResult">An instance of <see cref="IHttpResult{T}"/>.</param>
+        /// <returnsawaitable <see cref="Task{IActionResult}"/>.></returns>
+        public static IActionResult ToActionResult<T>(this IHttpResult<T> aHttpResult)
+            => aHttpResult.IsSuccess
+                   ? aHttpResult.Value.ToResultWithStatusCode(aHttpResult.StatusCode)
+                   : aHttpResult.ErrorList.GetErrorListAsString().ToResultWithStatusCode(aHttpResult.StatusCode);
 
         /// <summary>
         /// Creates and returns a new instance of <see cref="ResultWithStatusCode{T}"/> from the givn <typeparamref name="T"/> as <see cref="IActionResult"/>.
         /// </summary>
         /// <typeparam name="T">Type of the source object that will be used to create the resulting <see cref="ResultWithStatusCode{T}"/>.</typeparam>
-        /// <param name="aHttpResult">Instance of the source object that will be used to create the resulting <see cref="ResultWithStatusCode{T}".</param>
-        /// <param name="aStatusCode">HTTP StatusCode to be set in the returning <see cref="ResultWithStatusCode{T}".</param>
+        /// <param name="aObjectResultContextValue">Instance of the source object that will be used to create the resulting <see cref="ResultWithStatusCode{T}"/> from <see cref="ObjectResult".</param>
+        /// <param name="aObjectResultStatusCode">HTTP StatusCode to be set in the returning <see cref="ResultWithStatusCode{T}".</param>
         /// <returns><see cref="IActionResult"/></returns>
-        private static IActionResult ToHttpStatusCode<T>(this T aHttpResult, HttpStatusCode aStatusCode)
-        {
-            return new ResultWithStatusCode<T>(aHttpResult, aStatusCode);
-        }
+        private static IActionResult ToResultWithStatusCode<T>(this T aObjectResultContextValue, HttpStatusCode aObjectResultStatusCode)
+            => new ResultWithStatusCode<T>(aObjectResultContextValue, aObjectResultStatusCode);
+
+        /// <summary>
+        /// Gets a <see cref="Task"/> that returns a new instance of an <see cref="IActionResult"/> created from the provided <paramref name="aHttpResult"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the Value propery from <see cref="IHttpResult{T}"/>.</typeparam>
+        /// <param name="aHttpResult">An instance of <see cref="IHttpResult{T}"/>.</param>
+        /// <returns>awaitable <see cref="Task{IActionResult}"/>.</return
+        public static async Task<IActionResult> ToActionResult<T>(this Task<IHttpResult<T>> aHttpResult)
+            => (await aHttpResult).ToActionResult();
 
     }
 }
