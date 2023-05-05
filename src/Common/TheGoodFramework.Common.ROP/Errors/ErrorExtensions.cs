7f1f543343931ab7fc51fbffd14337f2bed65d9b
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-05-05 14:52:57 +0200

    ActionResult extensions rework to pick from Result value or Errors depending on the success or failure during the operation.

---
+++ src/Common/TheGoodFramework.Common.ROP/Errors/ErrorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGF.Common.ROP.Errors
+{
+    public static class ErrorExtensions
+    {
+        public static string GetHttpErrorListAsString(this IEnumerable<IHttpError> aHttpErrorList)
+        {
+            return string.Join(", ", aHttpErrorList);
+        }
+        public static string GetErrorListAsString(this IEnumerable<IError> aErrorList)
+        {
+            return string.Join(", ", aErrorList);
+        }
+    }
+}
