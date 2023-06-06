6390293691c65401d69723ef0e4a1ead392ac90a
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-06-06 15:34:52 +0200

    Code cleanup

--- src/CleanArchitecture/Infrastructure/TGF.CA.Infrastructure.Secrets/Vault/Vault_HealthCheck.cs
+++ src/CleanArchitecture/Infrastructure/TGF.CA.Infrastructure.Secrets/Vault/Vault_HealthCheck.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VaultSharp;
 
 namespace TGF.CA.Infrastructure.Secrets.Vault
 {
