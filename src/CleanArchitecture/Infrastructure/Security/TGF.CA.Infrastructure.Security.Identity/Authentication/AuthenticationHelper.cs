d52c8d4d94d59af7b65408b832452c6d21a739b5
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-07-26 20:59:28 +0200

    Updated Authentication abstractions with discord to work when the request passed through the APIGateway.

--- src/CleanArchitecture/Infrastructure/Security/TGF.CA.Infrastructure.Security.Identity/Authentication/AuthenticationHelper.cs
+++ src/CleanArchitecture/Infrastructure/Security/TGF.CA.Infrastructure.Security.Identity/Authentication/AuthenticationHelper.cs
@@ -29,5 +29,33 @@ namespace TGF.CA.Infrastructure.Security.Identity.Authentication
 
             return lDiscordUserAuth;
         }
+        /// <summary>
+        /// Replaces the value of the URL parameter "redirect_uri" with the provided string. 
+        /// </summary>
+        /// <param name="aOriginalUri">Original full Uri.</param>
+        /// <param name="aNewRedirectUri">Provided string that will replace the redirect_uri.</param>
+        /// <returns></returns>
+        public static string ReplaceRedirectUri(this string aOriginalUri, string aNewRedirectUri)
+        {
+            // Parse the original URL
+            UriBuilder lUriBuilder = new UriBuilder(aOriginalUri);
+
+            string lRedirectUriParamName = "redirect_uri=";
+            int lRedirectUriStart = aOriginalUri.IndexOf(lRedirectUriParamName);
+
+            int lRedirectUriEnd = aOriginalUri.IndexOf('&', lRedirectUriStart);
+            if (lRedirectUriEnd == -1) lRedirectUriEnd = aOriginalUri.Length;
+
+            string lRedirectUriParamNameAndValue = aOriginalUri.Substring(lRedirectUriStart, lRedirectUriEnd - lRedirectUriStart);
+
+            // Update the "redirect_uri" parameter
+            lUriBuilder.Query = lUriBuilder.Query.Replace(
+                lRedirectUriParamNameAndValue,
+                lRedirectUriParamName + Uri.EscapeDataString(aNewRedirectUri)
+            );
+
+            // Get the updated URL
+            return lUriBuilder.Uri.ToString();
+        }
     }
 }
