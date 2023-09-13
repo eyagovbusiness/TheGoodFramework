998676885033be4025294fa6f94c9cee1ca30606
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-09-13 20:28:49 +0200

    Modified CA.Infrastructure.Identity project: PermissionAuthorizationEndpointConventionBuilderExtensions.RequirePermissions() to take only one enum argument instead of several params.

--- src/CleanArchitecture/Infrastructure/Security/TGF.CA.Infrastructure.Identity/Authorization/Permission/PermissionAuthorizationEndpointConventionBuilderExtensions.cs
+++ src/CleanArchitecture/Infrastructure/Security/TGF.CA.Infrastructure.Identity/Authorization/Permission/PermissionAuthorizationEndpointConventionBuilderExtensions.cs
@@ -10,27 +10,24 @@ namespace TGF.CA.Infrastructure.Security.Identity.Authorization.Permission
     {
         /// <summary>
         /// Extension method of <see cref="IEndpointConventionBuilder"/> inspired on <see cref="AuthorizationEndpointConventionBuilderExtensions.RequireAuthorization{TBuilder}(TBuilder, string[])"/>.
-        /// This extension method emulates the same behaviour as the RequireAuthorization() method but instead of specifying policy names, enum values representing permissions are provided.
+        /// This extension method emulates the same behavior as the RequireAuthorization() method but instead of specifying policy names, flag-based enum values representing permissions are provided.
         /// The enum value expected is from an enum type defined with the attribute Flags.
-        /// When this method is called on an ednpoint, it creates internally a new <see cref="AuthorizationPolicy"/> that will be used to authorize this endpoint (the created policy will be reused for authorizing any other endpoints that requires the same set of permissions).
+        /// When this method is called on an endpoint, it creates internally a new <see cref="AuthorizationPolicy"/> that will be used to authorize this endpoint (the created policy will be reused for authorizing any other endpoints that requires the same set of permissions).
         /// </summary>
         /// <typeparam name="TBuilder">Builder derived from the endpoint that will require the permissions authorization.</typeparam>
         /// <typeparam name="TPermissionsEnum">The enum type that represents the permissions, the expected enum type should be defined with the attribute Flags.</typeparam>
         /// <param name="aBuilder"><see cref="IEndpointConventionBuilder"/> related to the endpoint that will require the permissions authorization.</param>
-        /// <param name="aPermissions"></param>
+        /// <param name="aPermissions">Required flag-based enum permissions.</param>
         /// <returns>The same instance of <see cref="IEndpointConventionBuilder"/> that can be used to chain more methods.</returns>
         /// <exception cref="ArgumentNullException">Can be thrown when aBuilder or aPermissions arguments are null.</exception>
-        public static TBuilder RequirePermissions<TBuilder, TPermissionsEnum>(this TBuilder aBuilder, params TPermissionsEnum[] aPermissions)
+        public static TBuilder RequirePermissions<TBuilder, TPermissionsEnum>(this TBuilder aBuilder, TPermissionsEnum aPermissions)
             where TBuilder : IEndpointConventionBuilder
             where TPermissionsEnum : struct
         {
             if (aBuilder == null)
                 throw new ArgumentNullException(nameof(aBuilder));
 
-            if (aPermissions == null)
-                throw new ArgumentNullException(nameof(aPermissions));
-
-            return aBuilder.RequireAuthorization(aPermissions.Select(permission => new AuthorizeAttribute(((int)(object)permission).ToString())).ToArray());
+            return aBuilder.RequireAuthorization(new AuthorizeAttribute(((int)(object)aPermissions).ToString()));
         }
     }
 }
