17383af9de386e0d6b9cd88159ae9b3540050407
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-01-02 16:33:26 +0100

    GSWB-68 Restored RabbitMQ healthChecks for .NET8

---
+++ src/CleanArchitecture/Infrastructure/Communication/TGF.CA.Infrastructure.Communication.RabbitMQ/RabbitMQHealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using HealthChecks.RabbitMQ;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using TGF.CA.Infrastructure.Communication.RabbitMQ.Settings;
+
+namespace TGF.CA.Infrastructure.Communication.RabbitMQ
+{
+    public class CusomRabbitMQHealthCheck(IRabbitMQSettingsFactory aRabbitMQSettingsFactory) : IHealthCheck
+    {
+        private readonly Lazy<Task<RabbitMQHealthCheck>> _rabbitMQHealthCheck = new(GetRabbitMQHealthCheck(aRabbitMQSettingsFactory));
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext aContext, CancellationToken aCancellationToken = default)
+        => await (await _rabbitMQHealthCheck.Value)
+            .CheckHealthAsync(aContext, aCancellationToken);
+
+        private static async Task<RabbitMQHealthCheck> GetRabbitMQHealthCheck(IRabbitMQSettingsFactory aRabbitMQSettingsFactory)
+        {
+            var lRabbitMQSettings = await aRabbitMQSettingsFactory.GetRabbitMQSettingsAsync();
+            var lRabbitMQHealthCheckOptions = new RabbitMQHealthCheckOptions() { ConnectionUri = new Uri(lRabbitMQSettings.GetConnectionString()) };
+            return new RabbitMQHealthCheck(lRabbitMQHealthCheckOptions);
+        }
+    }
+
+}
