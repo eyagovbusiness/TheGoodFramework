7768b7b3c82aa5772bf2ee4c69a6ec3237b41653
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-01-26 15:31:07 +0100

    GSWB-83 Added RetryUtility to Common.Extensions library.

---
+++ src/Common/TGF.Common.Extensions/RetryUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TGF.Common.Extensions
+{
+    public static class RetryUtility
+    {
+        public static async Task<TlResult> ExecuteWithRetryAsync<TlResult>(
+            Func<Task<TlResult>> aCondition,
+            Func<TlResult, bool> aRetryCondition,
+            int aMaxRetries = 3,
+            int aDelayMilliseconds = 1000,
+            CancellationToken aCancellationToken = default)
+        {
+            ArgumentNullException.ThrowIfNull(aCondition);
+            ArgumentNullException.ThrowIfNull(aRetryCondition);
+            int lRetryCount = 0;
+
+            do
+            {
+                try
+                {
+                    var lResult = await aCondition().ConfigureAwait(false);
+                    if (!aRetryCondition(lResult))
+                        return lResult;
+                }
+                catch (Exception)
+                {
+                    if (aCancellationToken.IsCancellationRequested || lRetryCount >= aMaxRetries)
+                        throw;
+                }
+
+                await Task.Delay(aDelayMilliseconds, aCancellationToken).ConfigureAwait(false);
+            }
+            while (++lRetryCount <= aMaxRetries && !aCancellationToken.IsCancellationRequested);
+
+            throw new InvalidOperationException("Max retry attempts exceeded.");
+        }
+    }
+
+
+}
