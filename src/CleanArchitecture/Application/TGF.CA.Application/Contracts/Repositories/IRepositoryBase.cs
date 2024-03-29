b3053ee16909eedff86da82a70fdcd2138aa27c1
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-03-29 20:31:39 +0100

    Added to the Application layer a generic DTO for paginated lists and an interface for CRUD methoid in the RepositoryBase from infrastructure layer.

---
+++ src/CleanArchitecture/Application/TGF.CA.Application/Contracts/Repositories/IRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGF.Common.ROP.HttpResult;
+
+namespace TGF.CA.Application.Contracts.Repositories
+{
+    /// <summary>
+    /// Interface of the base default CRUD implementations in TGF.CA.Infrastructure.DB.Repository.RepositoryBase class.
+    /// </summary>
+    public interface IRepositoryBase
+    {
+        public Task<IHttpResult<T>> AddAsync<T>(T aEntity, CancellationToken aCancellationToken = default)
+            where T : class;
+        public Task<IHttpResult<T>> GetByIdAsync<T>(object aEntityId, CancellationToken aCancellationToken = default)
+            where T : class;
+        public Task<IHttpResult<T>> UpdateAsync<T>(T aEntity, CancellationToken aCancellationToken = default)
+            where T : class;
+        public Task<IHttpResult<T>> DeleteAsync<T>(T aEntity, CancellationToken aCancellationToken = default)
+            where T : class;
+    }
+}
---
+++ src/CleanArchitecture/Application/TGF.CA.Application/DTOs/PaginatedListDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGF.CA.Application.DTOs
+{
+    public record PaginatedListDTO<T>(int CurrentPage, int TotalPages, int PageSize, int TotalCount, T[] List);
+}
