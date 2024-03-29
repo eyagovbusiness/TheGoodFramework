28bd2f8a8d98dd227b0414ef6b051e38280658be
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-03-29 20:31:39 +0100

    Added to the Application layer a generic DTO for paginated lists and an interface for CRUD methoid in the RepositoryBase from infrastructure layer.

--- src/CleanArchitecture/Application/TGF.CA.Application/DTOs/PaginatedListDTO.cs
+++ src/CleanArchitecture/Application/TGF.CA.Application/DTOs/PaginatedListDTO.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace TGF.CA.Application.DTOs
 {
+    /// <summary>
+    /// Genetric DTO for paginated lists.
+    /// </summary>
+    /// <typeparam name="T">Type of the items in the paginated list.</typeparam>
+    /// <param name="CurrentPage">The current page</param>
+    /// <param name="TotalPages">The total number of pages.</param>
+    /// <param name="PageSize">The page size</param>
+    /// <param name="TotalCount">The total count of items in this list.</param>
+    /// <param name="List">The list of paginated items.</param>
     public record PaginatedListDTO<T>(int CurrentPage, int TotalPages, int PageSize, int TotalCount, T[] List);
 }
