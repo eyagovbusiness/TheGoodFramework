a701944e3367c6208685aafeb372bcc3ed92bc16
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-06-17 21:11:52 +0200

    Added ApiRoutes

---
+++ src/CleanArchitecture/Presentation/TGF.CA.Presentation/ApiRoutes.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TGF.CA.Presentation
+{
+    public struct ApiRoutes
+    {
+        public struct Identifiers
+        {
+            public const string Id = "/{id}";
+            public const string Name = "/{name}";
+        }
+    }
+}
