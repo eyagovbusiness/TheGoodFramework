2ded37c8984bb87555d80540ff9ef183b7c1ae2f
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-07-10 22:28:29 +0200

    Added the option to include JWT bearer token in the HttpCommunicationService methods.

--- local_deploy.sh
+++ local_deploy.sh
@@ -7,7 +7,7 @@ Environment=development
 find . \( -name "*.csproj" -o -name "*.sln" -o -name "NuGet.docker.config" \) -print0 \
     | tar -cvf projectfiles.tar --null -T -
 
-docker build . --build-arg ENVIRONMENT=$Environment -t registry.guildswarm.org/$Environment/the_good_framework:latest
+docker build . --build-arg ENVIRONMENT=$Environment --no-cache -t registry.guildswarm.org/$Environment/the_good_framework:latest
 if [[ $0 == 1 || 0 ]]
 then
     rm projectfiles.tar
