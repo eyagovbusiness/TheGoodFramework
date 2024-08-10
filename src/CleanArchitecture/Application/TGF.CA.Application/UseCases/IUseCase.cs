8769970c34374f924fe41c0ffbfcde3ac1f91f2a
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-08-10 10:36:09 +0200

    Added an interface for the use case classes IUseCase and an extension method to add all use cases from assembly to DI.

---
+++ src/CleanArchitecture/Application/TGF.CA.Application/UseCases/IUseCase.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TGF.CA.Application.UseCases
+{
+    /// <summary>
+    /// Defines a use case with an asynchronous execution method.
+    /// </summary>
+    /// <typeparam name="TResponse">The type of the response produced by the use case.</typeparam>
+    /// <typeparam name="TRequest">The type of the request consumed by the use case.</typeparam>
+    public interface IUseCase<TResponse, TRequest>
+    {
+        /// <summary>
+        /// Executes the use case asynchronously.
+        /// </summary>
+        /// <param name="aRequest">The request object containing the parameters for the use case.</param>
+        /// <param name="aCancellationToken">A token to monitor for cancellation requests.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the response from the use case.</returns>
+        Task<TResponse> ExecuteAsync(TRequest aRequest, CancellationToken aCancellationToken = default);
+    }
+}
---
+++ src/CleanArchitecture/Application/TGF.CA.Application/UseCases/UseCaseRegistrationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace TGF.CA.Application.UseCases
+{
+    /// <summary>
+    /// Provides extension methods for registering use cases in the dependency injection container.
+    /// </summary>
+    public static class UseCaseRegistrationExtensions
+    {
+        /// <summary>
+        /// Registers all implementations of <see cref="IUseCase{TResponse, TRequest}"/> in the specified assembly with the dependency injection container.
+        /// </summary>
+        /// <param name="aServices">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <param name="aAssembly">The assembly to scan for use case implementations.</param>
+        /// <returns>The <see cref="IServiceCollection"/> for chaining.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when a use case implementation cannot be registered.</exception>
+        public static IServiceCollection AddUseCases(this IServiceCollection aServices, Assembly aAssembly)
+        {
+            // Cache the types from the assembly to avoid multiple enumeration
+            var lAllTypes = aAssembly.GetTypes();
+
+            // Find all types implementing IUseCase<TResponse, TRequest>
+            var lUseCaseTypes = lAllTypes
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IUseCase<,>))
+                    .Select(i => new { ImplementationType = t, InterfaceType = i }));
+
+            foreach (var lUseCase in lUseCaseTypes)
+            {
+                try
+                {
+                    aServices.AddScoped(lUseCase.InterfaceType, lUseCase.ImplementationType);
+                }
+                catch (Exception lEx)
+                {
+                    // Handle or log the exception as needed
+                    throw new InvalidOperationException($"Failed to register use case {lUseCase.ImplementationType.Name} for interface {lUseCase.InterfaceType.Name}", lEx);
+                }
+            }
+
+            return aServices;
+        }
+    }
+}
