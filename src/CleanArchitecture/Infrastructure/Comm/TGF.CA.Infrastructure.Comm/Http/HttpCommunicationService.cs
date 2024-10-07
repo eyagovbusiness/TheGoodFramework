1d45ef14f5c54e3a73765d05dddf12c89bff9300
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-10-07 22:55:24 +0200

    HttpCommunicationService rework to accept multiple authentication forwarding at the same time and of different types. Currently supporting JWT and Cookie authentication forwarding.

--- src/CleanArchitecture/Infrastructure/Comm/TGF.CA.Infrastructure.Comm/Http/HttpCommunicationService.cs
+++ src/CleanArchitecture/Infrastructure/Comm/TGF.CA.Infrastructure.Comm/Http/HttpCommunicationService.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
+using TGF.CA.Application.DTOs;
 using TGF.CA.Infrastructure.Discovery;
 using TGF.Common.ROP.Errors;
 using TGF.Common.ROP.HttpResult;
@@ -13,6 +14,7 @@ namespace TGF.CA.Infrastructure.Communication.Http
     /// <summary>
     /// Abstract class with logic for communicating with other cloud services via HTTP. Requires <see cref="IHttpClientFactory"/> and <see cref="IServiceDiscovery"/> from DI.
     /// </summary>
+    /// <remarks>In case of using AuthenticationForwardingDTO please remember that in case the AuthenticationForwardingDTO.AuthenticationType is fo type Cookie in the AuthenticationForwardingDTO.AuthenticationContent value the name of the cookie is expected to be as part of the content like "MyCookieName=auisgfduyiFVI"</remarks>
     public abstract class HttpCommunicationService(IServiceDiscovery aServiceDiscovery, IHttpClientFactory aHttpClientFactory, string aJsonMediaType = "application/json")
     {
         protected readonly IServiceDiscovery _serviceDiscovery = aServiceDiscovery;
@@ -21,43 +23,43 @@ namespace TGF.CA.Infrastructure.Communication.Http
 
         #region Protected
 
-        protected async Task<IHttpResult<TResponse>> GetAsync<TResponse>(string aServiceName, string aRequestUri, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task<IHttpResult<TResponse>> GetAsync<TResponse>(string aServiceName, string aRequestUri, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
-            var lResponse = await SendRequestAsync(HttpMethod.Get, aServiceName, aRequestUri, null, aAccessToken, aCancellationToken);
+            var lResponse = await SendRequestAsync(HttpMethod.Get, aServiceName, aRequestUri, authForwardings, null, aCancellationToken);
             return await GetResultAsync<TResponse>(lResponse, aCancellationToken);
         }
 
-        protected async Task PostAsync<TBody>(string aServiceName, string aRequestUri, TBody aRequestBody, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task PostAsync<TBody>(string aServiceName, string aRequestUri, TBody aRequestBody, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
             var lHttpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(aRequestBody), Encoding.UTF8, _jsonMediaType);
-            await SendRequestAsync(HttpMethod.Post, aServiceName, aRequestUri, lHttpContent, aAccessToken, aCancellationToken);
+            await SendRequestAsync(HttpMethod.Post, aServiceName, aRequestUri, authForwardings, lHttpContent, aCancellationToken);
         }
-        protected async Task<IHttpResult<TResponse>> PostAsync<TBody, TResponse>(string aServiceName, string aRequestUri, TBody aRequestBody, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task<IHttpResult<TResponse>> PostAsync<TBody, TResponse>(string aServiceName, string aRequestUri, TBody aRequestBody, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
             var lHttpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(aRequestBody), Encoding.UTF8, _jsonMediaType);
-            var lResponse = await SendRequestAsync(HttpMethod.Post, aServiceName, aRequestUri, lHttpContent, aAccessToken, aCancellationToken);
+            var lResponse = await SendRequestAsync(HttpMethod.Post, aServiceName, aRequestUri, authForwardings, lHttpContent, aCancellationToken);
             return await GetResultAsync<TResponse>(lResponse, aCancellationToken);
         }
 
-        protected async Task PutAsync<TBody>(string aServiceName, string aRequestUri, TBody aRequestBody, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task PutAsync<TBody>(string aServiceName, string aRequestUri, TBody aRequestBody, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
             var lHttpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(aRequestBody), Encoding.UTF8, _jsonMediaType);
-            await SendRequestAsync(HttpMethod.Put, aServiceName, aRequestUri, lHttpContent, aAccessToken, aCancellationToken);
+            await SendRequestAsync(HttpMethod.Put, aServiceName, aRequestUri, authForwardings, lHttpContent, aCancellationToken);
         }
-        protected async Task<IHttpResult<TResponse>> PutAsync<TBody, TResponse>(string aServiceName, string aRequestUri, TBody aRequestBody, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task<IHttpResult<TResponse>> PutAsync<TBody, TResponse>(string aServiceName, string aRequestUri, TBody aRequestBody, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
             var lHttpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(aRequestBody), Encoding.UTF8, _jsonMediaType);
-            var lResponse = await SendRequestAsync(HttpMethod.Put, aServiceName, aRequestUri, lHttpContent, aAccessToken, aCancellationToken);
+            var lResponse = await SendRequestAsync(HttpMethod.Put, aServiceName, aRequestUri, authForwardings, lHttpContent, aCancellationToken);
             return await GetResultAsync<TResponse>(lResponse, aCancellationToken);
         }
 
-        protected async Task DeleteAsync(string aServiceName, string aRequestUri, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task DeleteAsync(string aServiceName, string aRequestUri, IEnumerable<AuthenticationForwardingDTO>? authForwardings = default, CancellationToken aCancellationToken = default)
         {
-            await SendRequestAsync(HttpMethod.Delete, aServiceName, aRequestUri, null, aAccessToken, aCancellationToken);
+            await SendRequestAsync(HttpMethod.Delete, aServiceName, aRequestUri, authForwardings, null, aCancellationToken);
         }
-        protected async Task<IHttpResult<TResponse>> DeleteAsync<TResponse>(string aServiceName, string aRequestUri, string? aAccessToken = default, CancellationToken aCancellationToken = default)
+        protected async Task<IHttpResult<TResponse>> DeleteAsync<TResponse>(string aServiceName, string aRequestUri, IEnumerable<AuthenticationForwardingDTO>? authForwardings, CancellationToken aCancellationToken = default)
         {
-            var lResponse = await SendRequestAsync(HttpMethod.Delete, aServiceName, aRequestUri, null, aAccessToken, aCancellationToken);
+            var lResponse = await SendRequestAsync(HttpMethod.Delete, aServiceName, aRequestUri, authForwardings, null, aCancellationToken);
             return await GetResultAsync<TResponse>(lResponse, aCancellationToken);
         }
 
@@ -69,15 +71,28 @@ namespace TGF.CA.Infrastructure.Communication.Http
             HttpMethod method,
             string aServiceName,
             string aRequestUri,
+            IEnumerable<AuthenticationForwardingDTO>? authForwardings,
             HttpContent? content = null,
-            string? aAccessToken = default,
             CancellationToken aCancellationToken = default)
         {
             var lHttpClient = await GetHttpClientAsync(aServiceName);
             var lRequest = new HttpRequestMessage(method, aRequestUri) { Content = content };
 
-            if (!string.IsNullOrEmpty(aAccessToken))
-                lRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", aAccessToken);
+            if(authForwardings != null && authForwardings.Any())
+                foreach (var authForwarding in authForwardings)
+                    switch (authForwarding?.AuthenticationType)
+                    {
+                        case AuthenticationForwardingType.JWT:
+                            lRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authForwarding.AuthenticationContent);
+                            break;
+
+                        case AuthenticationForwardingType.Cookie:
+                            lRequest.Headers.Add("Cookie", authForwarding.AuthenticationContent);//Assumes the name of the cookie is part of the content like "MyCookieName=auisgfduyiFVI"
+                            break;
+
+                        default:
+                            throw new Exception($"Unsupported authentication type: {authForwarding?.AuthenticationType}");
+                    }
 
             return await lHttpClient.SendAsync(lRequest, aCancellationToken);
         }
