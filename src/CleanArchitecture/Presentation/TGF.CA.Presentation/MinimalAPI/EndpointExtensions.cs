f560f7b5062781adee6db3cb04466d6bdbd923ab
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-10-15 21:20:40 +0200

    Added endpoint route abstractions

---
+++ src/CleanArchitecture/Presentation/TGF.CA.Presentation/MinimalAPI/EndpointExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace TGF.CA.Presentation.MinimalAPI
+{
+    public static class EndpointExtensions
+    {
+        public static RouteHandlerBuilder MapDeleteRoute(this IEndpointRouteBuilder endpoints, RouteDefinition routeInfo, Delegate handler)
+        {
+            return endpoints.MapDelete(routeInfo.Route, handler)
+                            .WithName(routeInfo.OperationId);
+        }
+
+        public static RouteHandlerBuilder MapGetRoute(this IEndpointRouteBuilder endpoints, RouteDefinition routeInfo, Delegate handler)
+        {
+            return endpoints.MapGet(routeInfo.Route, handler)
+                            .WithName(routeInfo.OperationId);
+        }
+
+        public static RouteHandlerBuilder MapPostRoute(this IEndpointRouteBuilder endpoints, RouteDefinition routeInfo, Delegate handler)
+        {
+            return endpoints.MapPost(routeInfo.Route, handler)
+                            .WithName(routeInfo.OperationId);
+        }
+
+        public static RouteHandlerBuilder MapPutRoute(this IEndpointRouteBuilder endpoints, RouteDefinition routeInfo, Delegate handler)
+        {
+            return endpoints.MapPut(routeInfo.Route, handler)
+                            .WithName(routeInfo.OperationId);
+        }
+
+        public static RouteHandlerBuilder MapPatchRoute(this IEndpointRouteBuilder endpoints, RouteDefinition routeInfo, Delegate handler)
+        {
+            return endpoints.MapPatch(routeInfo.Route, handler)
+                            .WithName(routeInfo.OperationId);
+        }
+    }
+}
