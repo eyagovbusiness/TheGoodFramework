9ad0e41a638c235a30678188ad3fb5b70784a547
Damiano Robbiani <damiano.robbiani@gmail.com>
2024-11-06 09:44:57 +0100

    [SF-462] Fixed TGF build pipeline

--- local_deploy.sh
+++ local_deploy.sh
@@ -3,12 +3,52 @@ set -eux
 
 Environment=development
 
+# Default values
+NO_CACHE=false
+REGISTRY_PUSH=false
+IMAGE_REGISTRY=registry.guildswarm.org
+USER=guildswarm
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --no-cache) NO_CACHE=true ;;
+        --registry-push) REGISTRY_PUSH=true ;;
+		--image-registry-bgs) IMAGE_REGISTRY=biognosysbi.azurecr.io
+							  USER=biognosys		;;
+        --help) 
+            echo "Usage: $0 [--no-cache] [--registry-push]"
+            exit 0
+            ;;
+        *) 
+            # Ignore unknown options
+            ;;
+    esac
+    shift
+done
+
+
+# Check if required environment variables are set
+if [[ -z "${IMAGE_REGISTRY:-}" ]]; then
+    echo "Required environment variable IMAGE_REGISTRY is not set."
+    exit 1
+fi
+
+# Ensure the tarball is always removed, even if the script fails
+trap 'rm -f projectfiles.tar' EXIT
+
 # tarball csproj files, sln files, and NuGet.config
 find . \( -name "*.csproj" -o -name "*.sln" -o -name "NuGet.docker.config" \) -print0 \
     | tar -cvf projectfiles.tar --null -T -
 
-docker build . --build-arg ENVIRONMENT=$Environment --no-cache -t registry.guildswarm.org/$Environment/the_good_framework:latest
-if [[ $0 == 1 || 0 ]]
-then
-    rm projectfiles.tar
+# Build the Docker image with or without cache
+if [ "$NO_CACHE" = true ]; then
+	docker build --no-cache . --build-arg IMAGE_REGISTRY=${IMAGE_REGISTRY} --build-arg ENVIRONMENT=${Environment} --build-arg USER=${USER} -t $IMAGE_REGISTRY/base-images/$Environment/the_good_framework:latest
+else
+    docker build . --build-arg IMAGE_REGISTRY=${IMAGE_REGISTRY} --build-arg ENVIRONMENT=${Environment} --build-arg USER=${USER} -t $IMAGE_REGISTRY/base-images/$Environment/the_good_framework:latest
+fi
+
+# If the --registry-push argument is provided, push the image
+if [ "$REGISTRY_PUSH" = true ]; then
+    docker push $IMAGE_REGISTRY/base-images/$Environment/the_good_framework:latest
 fi
