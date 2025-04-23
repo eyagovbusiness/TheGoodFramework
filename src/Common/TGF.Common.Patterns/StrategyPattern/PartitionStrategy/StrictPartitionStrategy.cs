1514df7388e00c73839116ed32b5da1c18f31a4c
EduardoYagoVicent <Eduardo.Yago_Vicent@biognosys.com>
2025-04-23 14:37:40 +0200

    [SF-903] UPDATED EvenDistributionStrategy removing the micro-optimization to avoid barches of 1 file. It will be responsibilkituy of the user who uses this method to set the right value and do this optimization since addign this by default may cause more cofusion than good.

--- src/Common/TGF.Common.Patterns/StrategyPattern/PartitionStrategy/StrictPartitionStrategy.cs
+++ src/Common/TGF.Common.Patterns/StrategyPattern/PartitionStrategy/StrictPartitionStrategy.cs
@@ -1,10 +1,12 @@
 ﻿namespace TGF.Common.Patterns.StrategyPattern.PartitionStrategy {
+    /// <summary>
+    /// Implements a partition strategy that distributes elements strictly according to the specified partition size.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class StrictPartitionStrategy<T> : PartitionStrategyBase<T> {
-        protected override IEnumerable<IEnumerable<T>> PerformSplit(IEnumerable<T> source, int partitionSize, bool parallelize) {
-            return parallelize
+        protected override IEnumerable<IEnumerable<T>> PerformSplit(IEnumerable<T> source, int partitionSize, bool parallelize) => parallelize
                 ? DefaultPartitionParallel(source, partitionSize)
                 : DefaultPartition(source, partitionSize);
-        }
 
         private static IEnumerable<IEnumerable<T>> DefaultPartition(IEnumerable<T> source, int partitionSize) {
             using var enumerator = source.GetEnumerator();
@@ -13,15 +15,13 @@
             }
         }
 
-        private static IEnumerable<IEnumerable<T>> DefaultPartitionParallel(IEnumerable<T> source, int partitionSize) {
-            return source.AsParallel()
+        private static ParallelQuery<IEnumerable<T>> DefaultPartitionParallel(IEnumerable<T> source, int partitionSize) => source.AsParallel()
                 .Select((item, index) => new { item, index })
                 .GroupBy(x => x.index / partitionSize)
                 .Select(group => group.Select(x => x.item));
-        }
 
         private static IEnumerable<T> YieldPartition(IEnumerator<T> enumerator, int size) {
-            int count = 0;
+            var count = 0;
             do {
                 yield return enumerator.Current;
                 count++;
