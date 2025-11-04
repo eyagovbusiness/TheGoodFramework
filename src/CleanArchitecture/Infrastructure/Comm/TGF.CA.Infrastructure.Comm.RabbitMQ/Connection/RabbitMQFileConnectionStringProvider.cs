00c63f13d0ada6830dc6a27483fe2980c05a0d3d
Damiano Robbiani <damiano.robbiani@gmail.com>
2025-11-04 14:49:11 +0100

    [SF-1389] Changed key name

--- src/CleanArchitecture/Infrastructure/Comm/TGF.CA.Infrastructure.Comm.RabbitMQ/Connection/RabbitMQFileConnectionStringProvider.cs
+++ src/CleanArchitecture/Infrastructure/Comm/TGF.CA.Infrastructure.Comm.RabbitMQ/Connection/RabbitMQFileConnectionStringProvider.cs
@@ -5,6 +5,6 @@ namespace TGF.CA.Infrastructure.Comm.RabbitMQ.Connection;
 internal class RabbitMQFileConnectionStringProvider(ISecretFilesService secretFilesService)
 : IRabbitMQConnectionStringProvider {
     public async Task<string> GetConnectionString()
-    => await secretFilesService.GetSecretFromConfigAsync(InvariantConstants.ConfigurationKeys.SecretsFiles.SecretsFileNames.RabbitMQConnectionString);
+    => await secretFilesService.GetSecretFromConfigAsync(InvariantConstants.ConfigurationKeys.SecretsFiles.SecretsFileNames.RabbitMQCredentials);
 }
 
